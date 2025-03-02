8d83a23014eb1fb9aa35ea940987d164d644347a
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-02 01:02:32 +0100

    Create a race result upload endpoint

---
+++ src/GRL.Api/Controllers/RaceResultController.cs
@@ -0,0 +1,24 @@
+﻿using GRL.Api.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GRL.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RaceResultController : ControllerBase
+{
+    
+    [HttpPost("upload")]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> UploadRaceResult([FromForm] UploadRaceResultRequest? formModel)
+    {
+        if (formModel?.File.Length is 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
+
+        var file = formModel!.File;
+
+        return Ok(file);
+    }
+}
---
+++ src/GRL.Api/Dtos/UploadRaceResultRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GRL.Api.Dtos;
+
+public class UploadRaceResultRequest
+{
+    [Required] 
+    public IFormFile File { get; set; } = null!;
+    
+    public string League { get; set; }
+    
+    public string Season { get; set; }
+}
