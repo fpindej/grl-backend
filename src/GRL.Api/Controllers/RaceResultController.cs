9777a347deb0cf2cd0ffbd1b99fb1287b62314e3
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2025-03-02 01:21:35 +0100

    Check MIME type for csv

--- src/GRL.Api/Controllers/RaceResultController.cs
+++ src/GRL.Api/Controllers/RaceResultController.cs
@@ -7,17 +7,22 @@ namespace GRL.Api.Controllers;
 [Route("[controller]")]
 public class RaceResultController : ControllerBase
 {
-    
     [HttpPost("upload")]
     [Consumes("multipart/form-data")]
     public async Task<IActionResult> UploadRaceResult([FromForm] UploadRaceResultRequest? formModel)
     {
-        if (formModel?.File.Length is 0)
+        if (formModel?.File.Length is null or 0)
         {
             return BadRequest("No file uploaded.");
         }
 
-        var file = formModel!.File;
+        var file = formModel.File;
+
+        var isCsvFile = file.ContentType is "text/csv" or "application/vnd.ms-excel";
+        if (!isCsvFile)
+        {
+            return BadRequest("Invalid file type. Only CSV files are allowed.");
+        }
 
         return Ok(file);
     }
